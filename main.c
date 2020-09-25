526422968b4dc6612b04ac6e22a65ccac336777f
wasawin <wasawin.14@gmail.com>
2020-09-25 21:03:56 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  
+  int a,x;
+  printf("Enter A : ");   
+  scanf("%d",&a);
+  if(a>0)
+  x=a*15;
+  else
+  x=a*(-15);
+  printf("X = %d ",x);
+
+
+  return 0;
+}
